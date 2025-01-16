bd6a1dd7b1805674e005d25b86c102eeff4d7f04
Lazer Multifunction <lazermultifunction@gmail.com>
2025-01-17 00:34:35 +0700

    update 17/01/2025

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 
 
-GEMINI_API_KEY = "AIzaSyA1JZFMJMJJXMJV1hyNPqfPoOxLae0ZoLw"
+GEMINI_API_KEY = ""
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
